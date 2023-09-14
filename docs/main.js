0fac790ff239c25b49e127d5972f6dd517efa703
Yasika Hivin Gunathilaka <38648741+yasikahivin@users.noreply.github.com>
2023-09-14 16:35:49 +0530

    fix

--- docs/main.js
+++ docs/main.js
@@ -1404,7 +1404,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 const environment = {
     production: false,
-    trackAnalyticID: "G-XE9X54XCMH"
+    trackAnalyticID: "G-0ZF3M5YBRR",
 };
 /*
  * For easier debugging in development mode, you can import the following file
